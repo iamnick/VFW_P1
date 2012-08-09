49c75995e1ace03b1417f4d6c72d2bd83801ba00
Nick Stelzer <nickstelzer@Nicks-MacBook-Pro.local>
2012-08-08 21:31:17 -0400

    Added createTravelMethodList function

--- js/main.js
+++ js/main.js
@@ -10,10 +10,27 @@ window.addEventListener("DOMContentLoaded", function() {
 		var theElement = document.getElementById(x);
 		return theElement;
 	}
+	
+	// Create select field (dropdown list) element and populate with options
+	function createTravelMethodList () {
+		var formTag = document.getElementsByTagName("form"),
+			selectLi = $('select'),
+			makeSelect = document.createElement('select');
+			makeSelect.setAttribute("id", "travelMethod");
+		
+		for( var i = 0, j=travelMethods.length; i < j; i++) {
+			var makeOption = document.createElement('option');
+			var optText = travelMethods[i];
+			makeOption.setAttribute("value", optText);
+			makeOption.innerHTML = optText;
+			makeSelect.appendChild(makeOption);
+		}
+		selectLi.appendChild(makeSelect);
+	}
 
 	// Variable defaults
 	var travelMethods = ["Plane", "Train", "Car"];
-	
+	createTravelMethodList();
 	
 	// Set Link & Submit Click Events
 	var viewLink = $('viewAllTrips');
@@ -22,6 +39,13 @@ window.addEventListener("DOMContentLoaded", function() {
 	clearLink.addEventListener("click", clearData);
 	var addTrip = $('addTrip');
 	addTrip.addEventListener("click", storeData);
-	
-	
+
 });
+
+/*
+						<select id="travelMethod">
+							<option value="Plane">Plane</option>
+							<option value="Train">Train</option>
+							<option value="Car">Car</option>
+						</select>
+*/
