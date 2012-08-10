d8b5793fd49a14bf84adbb82442190b505c85e02
Nick Stelzer <nickstelzer@Nicks-MacBook-Pro.local>
2012-08-09 20:22:57 -0400

    finishing touches for project 2

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // Nick Stelzer
 // VFW 1208 - Project 2
-// August 8, 2012
+// August 9, 2012
 
 // Waits until DOM is ready
 window.addEventListener("DOMContentLoaded", function () {
@@ -142,11 +142,3 @@ window.addEventListener("DOMContentLoaded", function () {
 	peopleSlider.addEventListener("change", updatePeople);
 
 });
-
-/*
-						<select id="travelMethod">
-							<option value="Plane">Plane</option>
-							<option value="Train">Train</option>
-							<option value="Car">Car</option>
-						</select>
-*/
